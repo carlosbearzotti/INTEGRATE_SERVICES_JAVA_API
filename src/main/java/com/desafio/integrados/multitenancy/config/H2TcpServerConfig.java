99491459e5510419e5d7a554eb388f7e7d80205f
Carlos <bearzotti.ce@gmail.com>
2026-08-31 14:36:40 -0300

    feat: habilitar servidor TCP do H2 na porta 9092 para conexao direta com DBeaver

---
+++ src/main/java/com/desafio/integrados/multitenancy/config/H2TcpServerConfig.java
@@ -0,0 +1,23 @@
+package com.desafio.integrados.multitenancy.config;
+
+import org.h2.tools.Server;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.sql.SQLException;
+
+@Configuration
+@ConditionalOnClass(Server.class)
+public class H2TcpServerConfig {
+
+    /**
+     * Inicia o servidor TCP do H2 na porta 9092 permitindo conexões externas do DBeaver
+     * enquanto a aplicação Spring Boot estiver em execução.
+     */
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server h2TcpServer() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
+    }
+}
