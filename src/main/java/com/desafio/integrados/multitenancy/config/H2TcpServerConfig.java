37df93645af1b6ab6268ef9430c999e818e098f5
Carlos <bearzotti.ce@gmail.com>
2026-08-31 15:22:50 -0300

    fix: padronizar casing do schema H2 para compatibilidade com DBeaver e recriar tabelas

--- src/main/java/com/desafio/integrados/multitenancy/config/H2TcpServerConfig.java
+++ src/main/java/com/desafio/integrados/multitenancy/config/H2TcpServerConfig.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.File;
 import java.sql.SQLException;
 
 @Configuration
@@ -12,11 +13,21 @@ import java.sql.SQLException;
 public class H2TcpServerConfig {
 
     /**
-     * Inicia o servidor TCP do H2 na porta 9092 com permissão de conexões externas
-     * e flag -ifNotExists para compatibilidade total com o DBeaver.
+     * Inicia o servidor TCP do H2 na porta 9092 apontando o baseDir para a pasta ./data
+     * permitindo que o DBeaver conecte passando apenas 'integrados_db' como Database name.
      */
     @Bean(initMethod = "start", destroyMethod = "stop")
     public Server h2TcpServer() throws SQLException {
-        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092", "-ifNotExists");
+        File dataDir = new File("./data");
+        if (!dataDir.exists()) {
+            dataDir.mkdirs();
+        }
+        return Server.createTcpServer(
+                "-tcp",
+                "-tcpAllowOthers",
+                "-tcpPort", "9092",
+                "-baseDir", dataDir.getAbsolutePath(),
+                "-ifNotExists"
+        );
     }
 }
